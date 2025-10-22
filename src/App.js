21a9c55aa8079c7de83520b3dcd395982d2baeb8
Juan Esteban <102327436+JuanesVVV@users.noreply.github.com>
2025-10-22 00:22:06 +0000

    extensiones

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
-import Tableta from './componentes/table';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Tableta from './componentes/TableNew';
+import PokemonDetail from './componentes/PokemonDetail';
 
 function App() {
   return (
-    <div className="App">
-      <Tableta />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Tableta/>} />
+        <Route path='/pokemon/:name' element={<PokemonDetail/>}/>
+      </Routes>
+    </Router>
   );
 }
 
